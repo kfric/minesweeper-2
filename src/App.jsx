ef79813120c7d7a2f06ff95007941f51efa4bba8
Karl Frick <karl.frick5@gmail.com>
2021-05-28 14:11:00 -0400

    Add handleNewGame button that with 'POST' a new board

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import React, { Component } from 'react'
 export class App extends Component {
   state = {
     id: null,
-    //
     board: [
       ['_', '_', '_', '_', '_', '_', '_', '_'],
       ['_', '_', '_', '_', '_', '_', '_', '_'],
@@ -19,24 +18,40 @@ export class App extends Component {
     difficulty: 0,
   }
 
+  handleNewGame = async () => {
+    const body = { difficulty: 0 }
+    const response = await fetch(
+      `https://minesweeper-api.herokuapp.com/games`,
+      {
+        method: 'POST',
+        headers: { 'content-type': 'application/json' },
+        body: JSON.stringify(body),
+      }
+    )
+    const game = await response.json()
+    this.setState(game)
+  }
+
   render() {
+    // loop through the board array
+    // boardGrid = board.map - take every rowIndex and get the contents as 'row'
+    const boardGrid = this.state.board.map((row, rowIndex) => {
+      // foreach 'row' take every colIndex and get the contents of that 'cell'
+      return row.map((cell, colIndex) => {
+        // return each cell for each li
+        return <li key={colIndex}>{cell}</li>
+      })
+    })
     return (
       <main>
         <header>
           <h1>Minesweeper!</h1>
+          <p>{this.state.state}</p>
           <p>
-            <button>New Game</button>
+            <button onClick={this.handleNewGame}>New Game</button>
           </p>
         </header>
-        <ul>
-          {/* take this.state.board, take each row as rowIndex */}
-          {this.state.board.map((row, rowIndex) => {
-            // then foreach row, take each cell as colIndex
-            return row.map((cell, colIndex) => {
-              return <li></li>
-            })
-          })}
-        </ul>
+        <ul>{boardGrid}</ul>
       </main>
     )
   }
