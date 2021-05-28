a35ed1523fa1d9ff6c175d85fd9fec1fdc2fbd37
Karl Frick <karl.frick5@gmail.com>
2021-05-28 12:51:20 -0400

    .map the board to display all the rows/cols of the board from state

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,41 @@
 import React, { Component } from 'react'
 
 export class App extends Component {
+  state = {
+    id: null,
+    //
+    board: [
+      ['_', '_', '_', '_', '_', '_', '_', '_'],
+      ['_', '_', '_', '_', '_', '_', '_', '_'],
+      ['_', '_', '_', '_', '_', '_', '_', '_'],
+      ['_', '_', '_', '_', '_', '_', '_', '_'],
+      ['_', '_', '_', '_', '_', '_', '_', '_'],
+      ['_', '_', '_', '_', '_', '_', '_', '_'],
+      ['_', '_', '_', '_', '_', '_', '_', '_'],
+      ['_', '_', '_', '_', '_', '_', '_', '_'],
+    ],
+    state: 'new',
+    mines: 10,
+    difficulty: 0,
+  }
+
   render() {
     return (
       <main>
         <header>
           <h1>Minesweeper!</h1>
+          <p>
+            <button>New Game</button>
+          </p>
         </header>
         <ul>
-          <li>0</li>
-          <li>1</li>
-          <li>2</li>
-          <li>3</li>
-          <li>4</li>
-          <li>5</li>
-          <li>6</li>
-          <li>7</li>
-          <li>8</li>
+          {/* take this.state.board, take each row as rowIndex */}
+          {this.state.board.map((row, rowIndex) => {
+            // then foreach row, take each cell as colIndex
+            return row.map((cell, colIndex) => {
+              return <li></li>
+            })
+          })}
         </ul>
       </main>
     )
