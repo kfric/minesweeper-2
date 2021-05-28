9ff52550ba2a4cd8a36e6ddbc93f5477a443bbb9
Karl Frick <karl.frick5@gmail.com>
2021-05-28 00:02:02 -0400

    Basic HTML and CSS for 9 cells

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,23 @@ import React, { Component } from 'react'
 
 export class App extends Component {
   render() {
-    return <div>Hello, World!</div>
+    return (
+      <main>
+        <header>
+          <h1>Minesweeper!</h1>
+        </header>
+        <ul>
+          <li>0</li>
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+          <li>4</li>
+          <li>5</li>
+          <li>6</li>
+          <li>7</li>
+          <li>8</li>
+        </ul>
+      </main>
+    )
   }
 }
